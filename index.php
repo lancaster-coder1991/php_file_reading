55d55230d5a0eda551f6ae3c554be99904094a77
lancaster-coder1991 <grjscott1991@hotmail.com>
2020-12-27 13:50:00 +0000

    loop through array and create new objects that only include title, link and image info

--- index.php
+++ index.php
@@ -2,5 +2,7 @@
     $jsondata = file_get_contents("2020-01-02.json");
     $json = json_decode($jsondata, true);
     // print_r($json);
-    print_r($json[0]['files'][0]['title']);
+    // print_r($json[0]['files'][0]['title']);
+    $allButFirst = array_slice($json,1);
+    print_r($allButFirst[0]);
 ?>
