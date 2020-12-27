141676630b27278f9e720a54cdcbd9b07fd42f7c
lancaster-coder1991 <grjscott1991@hotmail.com>
2020-12-27 14:03:10 +0000

    add remaining information to php objects in stirp function

--- index.php
+++ index.php
@@ -4,5 +4,13 @@
     // print_r($json);
     // print_r($json[0]['files'][0]['title']);
     $allButFirst = array_slice($json,1);
-    print_r($allButFirst[0]);
+    // print_r($allButFirst[0]);
+
+    function stripInfo($obj) {
+        $strippedInfo = new stdClass();
+        $strippedInfo->title = $obj['attachments'][0]['title'];
+        return $strippedInfo;
+    }
+
+    print_r(array_map("stripInfo", $allButFirst)[1]);
 ?>
