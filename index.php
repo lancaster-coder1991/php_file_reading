3269be9350bde3912f0c6d129b08048ca1add7ef
lancaster-coder1991 <grjscott1991@hotmail.com>
2020-12-27 13:42:03 +0000

    continue using print function with associative array syntax to experiment with printing data from json file

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php  
     $jsondata = file_get_contents("2020-01-02.json");
     $json = json_decode($jsondata, true);
-    echo $json['test'][0]->type;
+    // print_r($json);
+    print_r($json[0]['files'][0]['title']);
 ?>
