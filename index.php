fc1b5138754937d3656e1478e2cc38a920eccdbb
lancaster-coder1991 <grjscott1991@hotmail.com>
2020-12-31 12:49:43 +0000

    finish commenting php

--- index.php
+++ index.php
@@ -63,7 +63,7 @@
         <div class="article-container">
              <h3 class="article-title"><a class="article-link" href=<?php echo($article->link)?>><?php print($article->title); ?> </a></h3>
            <?php  if(strpos($string,"200")) { ?>
-            <img class="article-image" src=<?php echo($article->image)?> alt=<?php echo("$article->service . image")?>>
+            <img class="article-image" src=<?php echo($article->image)?> alt="<?php echo $article->service?> image">
            <?php } ?>
         </div>
         <hr>
